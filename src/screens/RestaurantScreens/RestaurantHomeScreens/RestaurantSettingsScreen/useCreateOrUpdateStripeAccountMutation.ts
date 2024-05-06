0d8858dfea4b1f67b89b0117971141c29ed43bf1
Kristine McBride <kristinelmcbride@gmail.com>
2024-05-05 20:48:19 -0400

    stripe mutation

---
+++ src/screens/RestaurantScreens/RestaurantHomeScreens/RestaurantSettingsScreen/useCreateOrUpdateStripeAccountMutation.ts
@@ -0,0 +1,25 @@
+import type { MutationHookOptions } from "@apollo/client";
+import { gql, useMutation } from "@apollo/client";
+
+const CREATE_OR_UPDATE_STRIPE_ACCOUNT = gql`
+  mutation CreateOrUpdateStripeAccount {
+    createOrUpdateStripeAccount(input: {}) {
+      __typename
+      redirectLink
+      errors
+    }
+  }
+`;
+
+interface Data {
+  createOrUpdateStripeAccount: {
+    redirectLink: String;
+  };
+}
+
+type Options = MutationHookOptions<Data>;
+
+const useCreateOrUpdateStripeAccount = (options?: Options) =>
+  useMutation(CREATE_OR_UPDATE_STRIPE_ACCOUNT, options);
+
+export { useCreateOrUpdateStripeAccount };
