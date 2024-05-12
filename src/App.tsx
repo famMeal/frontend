75cd63321a7525a672946d7a0c6680f75b1d0f5b
ShahynKamali <shahynkamali@gmail.com>
2024-05-12 15:24:01 -0400

    added link

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ApolloProvider } from "@apollo/client";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import type { LinkingOptions } from "@react-navigation/native";
 import { NavigationContainer } from "@react-navigation/native";
 import { AppToast } from "components";
 import { COLOURS } from "constants/colours";
-import type { FC } from "react";
+import { useEffect, type FC } from "react";
+import { Linking } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import {
   AppSplashScreen,
@@ -19,11 +21,36 @@ import { client } from "./client";
 
 const { Navigator, Screen } = createBottomTabNavigator<RootStackParamList>();
 
+const linking: LinkingOptions<RootStackParamList> = {
+  prefixes: ["batch://"],
+  config: {
+    screens: {
+      Login: "login",
+      Splash: "splash",
+      SignUp: "signup",
+      EmailSignUp: "emailsignup",
+      VerifyAccount: "verifyaccount",
+      Clients: "clients",
+      Restaurants: "restaurants",
+    },
+  },
+};
+
 const App: FC = () => {
+  useEffect(() => {
+    const handleInitialURL = async () => {
+      const initialUrl = await Linking.getInitialURL();
+      if (initialUrl) {
+        console.log(`App opened with URL: ${initialUrl}`);
+      }
+    };
+
+    handleInitialURL();
+  }, []);
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ApolloProvider client={client}>
-        <NavigationContainer>
+        <NavigationContainer linking={linking}>
           <Navigator initialRouteName="Login">
             <Screen
               name="Login"
